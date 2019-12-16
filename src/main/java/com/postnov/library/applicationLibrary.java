bc517c5f21648c78b6795766f5cb97f2429fdd34
Anatoliy Postnov <postnov-90@mail.ru>
2019-12-16 15:53:34 +0300

    template library

---
+++ src/main/java/com/postnov/library/applicationLibrary.java
@@ -0,0 +1,11 @@
+package com.postnov.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class applicationLibrary {
+    public static void main(String[] args){
+        SpringApplication.run(applicationLibrary.class, args);
+    }
+}
